bfa4dbf0e2f09e7f5b2df1b08bbd03c9d88030d2
Poojan <poojangoyani@gmail.com>
2024-12-20 01:18:05 +0530

    `Added navigation and styling to EditProductPage component`

--- src/app/admin/products/[id]/page.tsx
+++ src/app/admin/products/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { notFound } from "next/navigation";
 import { getCachedSingleProduct } from "../_actions/products";
 import ProductForm from "../_components/product-form/ProductForm";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
 interface EditProductPageProps {
   params: Promise<{ id: string }>;
 }
@@ -13,8 +16,18 @@ export default async function EditProductPage({
   const product = await getCachedSingleProduct(id);
   if (!product) return notFound();
   return (
-    <>
-      <ProductForm product={product} />
-    </>
+    <Card className="border-none  shadow-none bg-background ">
+      <CardHeader className="max-w-5xl mx-auto">
+        <CardTitle className=" text-left flex items-center gap-2">
+          <Link href="/admin/products">
+            <ChevronLeft />
+          </Link>
+          Edit Product
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ProductForm product={product} />
+      </CardContent>
+    </Card>
   );
 }
