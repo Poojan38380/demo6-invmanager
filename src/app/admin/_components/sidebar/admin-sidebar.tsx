d687ff5b408755f13a9487afd54fb23ecdc07503
Poojan <poojangoyani@gmail.com>
2024-12-27 16:46:07 +0530

    changed logo once again

--- src/app/admin/_components/sidebar/admin-sidebar.tsx
+++ src/app/admin/_components/sidebar/admin-sidebar.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
-import { Command } from "lucide-react";
-
+import Image from "next/image";
 import {
   Sidebar,
   SidebarContent,
@@ -13,15 +12,22 @@ import { PrimaryNav } from "./primary-nav";
 import { SecondaryNav } from "./secondary-nav";
 import { NavUser } from "./nav-user";
 
-export async function AppSidebar({
-  ...props
-}: React.ComponentProps<typeof Sidebar>) {
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar variant="inset" {...props}>
       <SidebarHeader className="pt-3">
-        <a href="/admin" className="grow flex gap-2">
-          <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-            <Command className="size-4" />
+        <a
+          href="/admin"
+          className="grow flex gap-2"
+          aria-label="Go to Inv Manager Admin Dashboard"
+        >
+          <div className="flex aspect-square size-8 items-center justify-center rounded-lg">
+            <Image
+              src={"/logo.svg"}
+              alt="Inv Manager Logo"
+              width={32}
+              height={32}
+            />
           </div>
           <div className="grid flex-1 text-left text-sm leading-tight">
             <span className="truncate font-semibold">Inv Manager</span>
