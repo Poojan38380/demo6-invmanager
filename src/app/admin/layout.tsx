11cda24bd2d8fb5f40aba3ccc6b5ffcb1072fa6b
Poojan <poojangoyani@gmail.com>
2024-12-29 00:49:59 +0530

    changed sidebar icon in admin panel

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -17,9 +17,9 @@ export default function AdminLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset className="overflow-x-auto   ">
-        <header className="flex h-14 shrink-0 items-center gap-2">
+        <header className="flex  border-b h-14 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4 w-full">
-            <SidebarTrigger className="-ml-1 scale-125" />
+            <SidebarTrigger className=" scale-125" />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <div className="flex justify-between w-full items-center">
               <BreadCrumbs />
