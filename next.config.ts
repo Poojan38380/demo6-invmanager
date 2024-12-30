255fa04cd68f67725fd6b7f386df933e83483637
Poojan <poojangoyani@gmail.com>
2024-12-31 01:15:30 +0530

    configured meta data and added some fields in next config pwa

--- next.config.ts
+++ next.config.ts
@@ -59,6 +59,8 @@ const nextConfig: NextConfig = {
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  register: true,
+  skipWaiting: true,
   disable: process.env.NODE_ENV === "development", // Disable PWA in development
 });
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,53 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { SessionProvider } from "next-auth/react";
 
+import type { Metadata, Viewport } from "next";
+
+const APP_NAME = "InvManager";
+const APP_DEFAULT_TITLE = "InvManager";
+const APP_TITLE_TEMPLATE = "%s - App";
+const APP_DESCRIPTION = "Manage your inventory seamlessly!";
+
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#f1f1f1",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
